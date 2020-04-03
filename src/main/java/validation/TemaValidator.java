47d99e1e10b74069492ed6cdd759acab17c438dd
Bogdan Savianu <savianubogdan@gmail.com>
2020-04-03 14:46:51 +0300

    assignment test

--- src/main/java/validation/TemaValidator.java
+++ src/main/java/validation/TemaValidator.java
@@ -6,21 +6,22 @@ public class TemaValidator implements Validator<Tema> {
 
     /**
      * Valideaza o tema
+     *
      * @param entity - tema pe care o valideaza
      * @throws ValidationException daca tema nu e valida
      */
     @Override
     public void validate(Tema entity) throws ValidationException {
-        if(entity.getID().equals("") || entity.getID() == null) {
+        if (entity.getID() == null || entity.getID().equals("")) {
             throw new ValidationException("Numar tema invalid!");
         }
-        if(entity.getDescriere().equals("")){
+        if (entity.getDescriere().equals("")) {
             throw new ValidationException("Descriere invalida!");
         }
-        if(entity.getDeadline() < 1 || entity.getDeadline() > 14) {
+        if (entity.getDeadline() < 1 || entity.getDeadline() > 14) {
             throw new ValidationException("Deadlineul trebuie sa fie intre 1-14.");
         }
-        if(entity.getPrimire() < 1 || entity.getPrimire() > 14) {
+        if (entity.getPrimire() < 1 || entity.getPrimire() > 14) {
             throw new ValidationException("Saptamana primirii trebuie sa fie intre 1-14.");
         }
     }
---
+++ src/test/java/assignment/AssignmentServiceTest.java
@@ -0,0 +1,48 @@
+package assignment;
+
+import domain.Student;
+import domain.Tema;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import repository.NotaXMLRepo;
+import repository.StudentXMLRepo;
+import repository.TemaXMLRepo;
+import service.Service;
+import validation.NotaValidator;
+import validation.StudentValidator;
+import validation.TemaValidator;
+import validation.ValidationException;
+
+public class AssignmentServiceTest {
+    private static Service service;
+
+    @BeforeClass
+    public static void before() {
+        StudentValidator studentValidator = new StudentValidator();
+        TemaValidator temaValidator = new TemaValidator();
+        String filenameStudent = "src/test/resources/fisiere/Studenti.xml";
+        String filenameTema = "src/test/resources/fisiere/Teme.xml";
+        String filenameNota = "src/test/resources/fisiere/Note.xml";
+
+        StudentXMLRepo studentXMLRepository = new StudentXMLRepo(filenameStudent);
+        TemaXMLRepo temaXMLRepository = new TemaXMLRepo(filenameTema);
+        NotaValidator notaValidator = new NotaValidator(studentXMLRepository, temaXMLRepository);
+        NotaXMLRepo notaXMLRepository = new NotaXMLRepo(filenameNota);
+        service = new Service(studentXMLRepository, studentValidator, temaXMLRepository, temaValidator,
+                notaXMLRepository, notaValidator);
+    }
+
+    @Test(expected = ValidationException.class)
+    public void addAssignmentInvalidId() {
+        Tema assignment = new Tema(null, "testValid", 4, 3);
+        service.addTema(assignment);
+    }
+
+    @Test
+    public void addAssignmentValidId() {
+        Tema assignment = new Tema("200", "testValid", 4, 3);
+        Tema result = service.addTema(assignment);
+        Assert.assertEquals(assignment.getID(), result.getID());
+    }
+}
